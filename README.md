2e61dbead66c96ef3891893d22ed271f496a4dec
Hao Le <hao.levoquoc2010@gmail.com>
2026-01-02 22:14:25 -0600

    Added multiple choice editor option

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ LinkMap currently supports 5 assessment item types:
 # Multiple Choice
 The user is provided with a question and a list of answers.
 User select ONE answer as the correct answer.
+
+In editor mode, separate the answers using commas with the first one being correct.  
+Example: "answer1,answer2,answer3,answer4,answer5" - "answer1" is the correct answer
--- blob/master/modes/editor.py
+++ blob/master/modes/editor.py
@@ -30,5 +30,32 @@ def editor_mode(data, end):
         data[subject][quiz] = {}
 
     finished_adding_questions = False
+    last_type_of_assessment_item = None
+    quiz_questions_dictionary = {}
 
     while not finished_adding_questions:
+        if last_type_of_assessment_item is None:
+            last_type_of_assessment_item = input(f"Choose one type of assessment items to add:\n"
+                                                 f"(MC)  Multiple Choice\n"
+                                                 f"(SDR) Selected-Response\n"
+                                                 f"(M)   Matching\n"
+                                                 f"(SR)  Short Response\n"
+                                                 f"(S)   Sequence\n").strip().lower()
+        else:
+            if input("Do you want to add another question? (Y/N) ").strip().lower() == "n":
+                data[subject][quiz] = quiz_questions_dictionary
+                end()
+
+            if input("Do you want to continue adding the same type of assessment items? (Y/N)").strip().lower() == "n":
+                last_type_of_assessment_item = input(f"Choose one type of assessment items to add:\n"
+                                                     f"(MC)  Multiple Choice\n"
+                                                     f"(SDR) Selected-Response\n"
+                                                     f"(M)   Matching\n"
+                                                     f"(SR)  Short Response\n"
+                                                     f"(S)   Sequence").strip().lower()
+
+
+        if last_type_of_assessment_item == "mc":
+            question = input("Question: ")
+            quiz_questions_dictionary[question] = input("Answers: ").strip().split(",") + ["MC"]
+
